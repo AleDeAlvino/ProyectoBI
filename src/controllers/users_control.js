f8b1fb3f08a1b1b35922522ddaa9196120b544cf
Ale De Alvino <alejandra.dealvino.melgoza@gmail.com>
2022-05-29 01:03:45 -0500

    Login

--- src/controllers/users_control.js
+++ src/controllers/users_control.js
@@ -1,7 +1,7 @@
 // const jwt = require('jsonwebtoken');
 const db = require('../../config/database');
 var bcrypt = require('bcrypt');
-// const {secret} = require('../../config/config');
+const {secret} = require('../../config/config');
 
 // import {Fecha_actual} from '../funciones_js/validaciones';
 
@@ -46,10 +46,46 @@ export const register = async (req, res) => {
         res.status(400).json({code: 400, message: 'Campos incompletos'});
     }
 
-//     ///codigos de respuesta . . .
-//     //200: usuario autenticado
-//     //400: error del usuario
-//     //500: error del servidor
+    ///codigos de respuesta . . .
+    //200: usuario autenticado
+    //400: error del usuario
+    //500: error del servidor
 }
 
+//funcion para login
+export const login = async (req, res) => {
+    const { Username, Password } = req.body;
+    let query = "SELECT * FROM usuarios WHERE Username = ?";
+    let result = await db.query(query, [Username]);
 
+    //comprobar que los campos esten completos
+    if(Username && Password){
+        //validar que existe el usuario
+        if(result.length > 0){
+            //validar que la contraseña sea correcta
+            if(bcrypt.compareSync(Password, result[0].Password)){
+                //generar token
+                const token = jwt.sign({
+                    Username: result[0].Username,
+                    Usuario_id: result[0].Socio_id
+                }, secret);
+
+                //mandando token por el header
+                res.status(200)
+                    .header('Authorization', token)
+                    .json({ code: 200, message: 'Usuario autenticado'})
+            }
+        }
+        else{
+            //usuario no existe
+            res.status(400).json({code: 400, message: 'Usuario no existe'});
+        }
+    }else{
+        //campos incompletos
+        res.status(400).json({code: 400, message: 'Campos incompletos'});
+    }
+    //codigos de respuesta . . .
+    //200: usuario autenticado
+    //400: error del usuario
+    //500: error del servidor
+}
